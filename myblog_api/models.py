3fb51c6e0debfec508a2544f53475964824f6f4b
KevinPercy <kevinpercycl@gmail.com>
2020-05-28 11:00:26 -0500

    Created userprofiles and manager, and aplied migrations

--- myblog_api/models.py
+++ myblog_api/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, last_name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)  # takes care of the second half        
+        user = self.model(email=email, name=name, last_name=last_name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, last_name, password):
+        """Create and save a new superuser with given details """
+        user = self.create_user(email, name, last_name, password)
+
+        user.is_superuser = True #automatically create in the PermissionsMixin
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        """Return the string representation of our user"""
+        return self.email
--- myblog_project/settings.py
+++ myblog_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.auth',
+    'rest_framework.authtoken',
     'myblog_api',
 ]
 
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'myblog_api.UserProfile'
