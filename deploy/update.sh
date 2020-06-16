59f77cb8d2c12cf43d819298b13e358763b0f7bd
KevinPercy <kevinpercycl@gmail.com>
2020-06-15 19:17:00 -0500

    Changed update.sh file

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,6 +7,6 @@ PROJECT_BASE_PATH='/usr/local/apps/myblog-rest-api'
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-supervisorctl restart profiles_api
+supervisorctl restart myblog_api
 
 echo "DONE! :)"
